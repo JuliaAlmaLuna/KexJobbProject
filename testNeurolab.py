e1544bc2d616a0d4bd836eac09c42b555be4185a
JuliaAlmaLuna <jonerud@kth.se>
2020-03-25 14:41:31 +0100

    testCommit

---
+++ testNeurolab.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+""" 
+Example of use singe layer perceptron(newp)
+===========================================
+
+Train with Delta rule
+
+"""
+
+import neurolab as nl
+
+# Logical &
+input = [[0, 0], [0, 1], [2, 0], [2, 2], [2, 1]]
+target = [[0], [0], [1], [1], [1]]
+
+test = [[0, 1], [1, 1], [1, 0], [1, 1]]
+
+# Create net with 2 inputs and 1 neuron
+net = nl.net.newp([[0, 2],[0, 1]], 1)
+
+# train with delta rule
+# see net.trainf
+error = net.train(input, target, epochs=100, show=10, lr=0.1)
+
+print(net.sim(test))
+
+# Plot results
+
+import pylab as pl
+pl.plot(error)
+pl.xlabel('Epoch number')
+pl.ylabel('Train error')
+pl.grid()
+pl.show()
