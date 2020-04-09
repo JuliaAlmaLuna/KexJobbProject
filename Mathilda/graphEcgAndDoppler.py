9f007bbae95b9f09dbc58003e8bbe94cc51b1a38
mberli <mberli@kth.se>
2020-04-09 19:07:32 +0200

    added stuff to mlp

--- Mathilda/graphEcgAndDoppler.py
+++ Mathilda/graphEcgAndDoppler.py
@@ -4,44 +4,70 @@ import matplotlib.pyplot as plt
 
 # This file plots all ecg and doppler in graphs together (different scales) to check for inconsistencies in time
 
-file_folder = "../MLPregressor_sklearn/ECG_Folder/"
-file_names = [file_folder + "2_ecg.txt", file_folder + "3_ecg.txt", file_folder + "6_ecg.txt", file_folder +
-              "7_ecg.txt", file_folder + "4_ecg.txt", file_folder + "9_ecg.txt", file_folder + "11_ecg.txt",
-              file_folder + "5_ecg.txt", file_folder + "8_ecg.txt"]
+file_folder = "ecg_Folder/"
+number_of_samples = 97
 
 
+def init_file_names(number_of_samples, file_folder_, suffix):
+    file_names_ = []
+
+    for index in range(number_of_samples):
+        string = file_folder_ + str(index+1) + suffix
+        file_names_.append(string)
+    return file_names_
+
+
+file_names = init_file_names(number_of_samples, file_folder, "_ecg.txt")
+print(file_names)
+
+
+# If data is upside-down it is fixed
 def get_data(f_names):
     targets = []
     inputs = []
 
     for name in f_names:
-        f_ecg, f_v, x = dmf.create_interpolation_function(name, 0.02, 2.31, 950)
-        targets.append(f_ecg(x))
-        inputs.append(f_v(x))
-
+        ecg, v = dmf.import_td_text_file(name)
+        start = v[0, 0]
+        end = v[len(v)-1, 0]*0.95
+        f_ecg, f_v, x = dmf.create_interpolation_function_ecg_v(ecg, v, start, end, 500)
+        if name == "ecg_Folder/9_ecg.txt" or name == "ecg_Folder/19_ecg.txt" or name == "ecg_Folder/62_ecg.txt" or name == "ecg_Folder/65_ecg.txt" or name == "ecg_Folder/69_ecg.txt" or name == "ecg_Folder/92_ecg.txt":
+            targets.append(-f_ecg(x))
+            inputs.append(f_v(x))
+        else:
+            targets.append(f_ecg(x))
+            inputs.append(f_v(x))
     targets = np.array(targets)
     inputs = np.array(inputs)
     return targets, inputs, x
 
 
-targets, inputs, x = get_data(file_names)
-for index in range(9):
-    input_ = inputs[index, :]
-    target = targets[index, :]
+def graph_ecg_and_doppler(x, ecg, v, name):
     fig, ax1 = plt.subplots()
 
     color = 'tab:red'
-    ax1.set_xlabel('time (s)')
-    ax1.set_ylabel('exp', color=color)
-    ax1.plot(x, input_, color=color)
+    ax1.set_xlabel(name)
+    ax1.set_ylabel('velocity', color=color)
+    ax1.plot(x, v, color=color)
     ax1.tick_params(axis='y', labelcolor=color)
 
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
     color = 'tab:blue'
-    ax2.set_ylabel('sin', color=color)  # we already handled the x-label with ax1
-    ax2.plot(x, target, color=color)
+    ax2.set_ylabel('ecg', color=color)  # we already handled the x-label with ax1
+    ax2.plot(x, ecg, color=color)
     ax2.tick_params(axis='y', labelcolor=color)
 
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
     plt.show()
+
+
+targets, inputs, x = get_data(file_names)
+for index in range(100):
+    input_ = inputs[index, :]
+    target = targets[index, :]
+    graph_ecg_and_doppler(x, target, input_, file_names[index])
+
+
+
+
--- Mathilda/mlpAlgorithmMathilda.py
+++ Mathilda/mlpAlgorithmMathilda.py
@@ -6,10 +6,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import explained_variance_score
 from sklearn.metrics import max_error
+from sklearn.metrics import r2_score
 from scipy.stats import pearsonr
 from Mathilda import genericEcg as ge
 
-number_of_sample_data = 100
+number_of_sample_data = 97
 file_folder = "Mathilda/ecg_folder/"
 suffix = "_ecg.txt"
 
@@ -42,6 +43,7 @@ def loop_data(ecg, v, end_time):
     return ecg, v
 
 
+# If ecg is upside-down it is fixed
 def get_data(f_names, end_time):
     targets = []
     inputs = []
@@ -50,8 +52,12 @@ def get_data(f_names, end_time):
         ecg, v = dmf.import_td_text_file(name)
         ecg, v = loop_data(ecg, v, end_time)
         f_ecg, f_v, x = dmf.create_interpolation_function_ecg_v(ecg, v, 0.035, end_time, 500)
-        targets.append(f_ecg(x))
-        inputs.append(f_v(x))
+        if name == "ecg_Folder/9_ecg.txt" or name == "ecg_Folder/19_ecg.txt" or name == "ecg_Folder/62_ecg.txt" or name == "ecg_Folder/65_ecg.txt" or name == "ecg_Folder/69_ecg.txt" or name == "ecg_Folder/92_ecg.txt":
+            targets.append(-f_ecg(x))
+            inputs.append(f_v(x))
+        else:
+            targets.append(f_ecg(x))
+            inputs.append(f_v(x))
 
     targets = np.array(targets)
     inputs = np.array(inputs)
@@ -72,13 +78,6 @@ def graph_predictions(mlp, testing_inputs, testing_targets, x, rows, columns):
         plt.show()
 
 
-mlp = MLPRegressor(
-    hidden_layer_sizes=(100,),  activation='tanh', solver='lbfgs', alpha=0.001, batch_size='auto',
-    learning_rate='constant', learning_rate_init=0.01, power_t=0.5, max_iter=100000, shuffle=True,
-    random_state=9, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True,
-    early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-
-
 def train_with_generic_ecg(mlp, _inputs, t):
     generic_ecg = ge.cut_generic_ecg_(t)
     generic_ecgs = []
@@ -90,13 +89,18 @@ def train_with_generic_ecg(mlp, _inputs, t):
 
 
 def evaluate_performance(mlp, testing_inputs, testing_targets, training_inputs, training_targets, message=""):
-    r2_score = mlp.score(testing_inputs, testing_targets)
     train_prediction = mlp.predict(training_inputs)
     test_prediction = mlp.predict(testing_inputs)
+    r2_score_ = mlp.score(testing_inputs, testing_targets)
     ev_score = explained_variance_score(testing_inputs, test_prediction, multioutput='uniform_average')
     train_mse = mean_squared_error(training_targets, train_prediction)
     test_mse = mean_squared_error(testing_targets, test_prediction)
-    pearson_corr, _ = pearsonr(testing_targets[0, :], test_prediction[0, :])
+
+    total = 0  # Finding the mean pearson score of testing data
+    for index in range(len(testing_inputs)):
+        pearson_corr, _ = pearsonr(testing_targets[index, :], test_prediction[index, :])
+        total = total + pearson_corr
+    pearson = total/len(testing_inputs)
 
     curr_max = 0  # Finding the maximum error in all the training data
     for index in range(len(testing_inputs)):
@@ -106,19 +110,56 @@ def evaluate_performance(mlp, testing_inputs, testing_targets, training_inputs, 
 
     print(message)
     print("__________________________________________________________________________")
-    print("R2 score:\t\t\t" + str(r2_score) + "\t\t\tGoal: positive & close to 1" + "\ntest MSE:\t\t\t" + str(test_mse)
+    print("R2 score:\t\t\t" + str(r2_score_) + "\t\t\tGoal: positive & close to 1" + "\ntest MSE:\t\t\t" + str(test_mse)
           + "\t\t\tGoal: lower than train MSE" + "\ntrain MSE:\t\t\t" + str(train_mse) + "\nEVS, ua:\t\t\t" +
           str(ev_score) + "\t\t\tGoal: Same as R2" + "\nMax error:\t\t\t" + str(curr_max) +
-          "\t\t\tGoal: As low as possible" + "\nPearson:\t\t\t" + str(pearson_corr) + "\t\t\tGoal: close to 1")
+          "\t\t\tGoal: As low as possible" + "\nPearson:\t\t\t" + str(pearson) + "\t\t\tGoal: close to 1")
     print("__________________________________________________________________________")
 
 
 file_names = init_file_names(number_of_sample_data, file_folder, suffix)
 targets, inputs, X = get_data(file_names, 3)
-training_inputs, testing_inputs, training_targets, testing_targets = train_test_split(inputs, targets, test_size=0.3, random_state=42)
+training_inputs, testing_inputs, training_targets, testing_targets = train_test_split(inputs, targets, test_size=0.3, random_state=66)
+
+
+def find_best_beta(training_inputs_, training_targets_, testing_inputs_, testing_targets_):
+    best_r2 = -100
+    best_beta1 = 0
+    best_beta2 = 0
+    for beta1 in np.arange(0, 1, 0.1):
+        for beta2 in np.arange(0, 1, 0.1):
+            mlp = MLPRegressor(
+                hidden_layer_sizes=(50,), activation='tanh', solver='adam', alpha=10, batch_size='auto',
+                learning_rate='constant', learning_rate_init=0.01, power_t=0.5, max_iter=1000, shuffle=True,
+                random_state=9, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True,
+                early_stopping=True, validation_fraction=0.2, beta_1=beta1, beta_2=beta2, epsilon=1e-08)
+            mlp.fit(training_inputs_, training_targets_)
+            score = mlp.score(testing_inputs_, testing_targets_)
+            if score > best_r2:
+                best_r2 = score
+                best_beta1 = beta1
+                best_beta2 = beta2
+    print("beta 1: ")
+    print(best_beta1)
+    print("beta 2: ")
+    print(best_beta2)
+    print("r2: ")
+    print(best_r2)
+
+
+mlp = MLPRegressor(
+    hidden_layer_sizes=(50,), activation='tanh', solver='adam', alpha=0.0001, batch_size='auto',
+    learning_rate='constant', learning_rate_init=0.01, power_t=0.5, max_iter=1000, shuffle=True,
+    random_state=99, tol=0.0001, verbose=True, warm_start=False, momentum=0.9, nesterovs_momentum=True,
+    early_stopping=True, validation_fraction=0.1, beta_1=0.8, beta_2=0.6, epsilon=1e-08)
 mlp.fit(training_inputs, training_targets)
 graph_predictions(mlp, testing_inputs=testing_inputs, testing_targets=testing_targets, x=X, rows=5, columns=7)
 evaluate_performance(mlp, testing_inputs, testing_targets, training_inputs, training_targets)
+
+
+
+
+
 
 
 
